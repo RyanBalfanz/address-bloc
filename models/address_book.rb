3af6dc4a74cb06babf5a2dbba5c50b4c1ed4f694
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-09-09 21:47:09 -0700

    Checkpoint 19; Assignment
    ```
    .....FF.
    
    Failures:
    
      1) AddressBook#remove_entry removes a single entry from the address book
         Failure/Error: expect(book.entries.size).to eq(0)
    
           expected: 0
                got: 1
    
           (compared using ==)
         # ./spec/address_book_spec.rb:46:in `block (3 levels) in <top (required)>'
    
      2) AddressBook#remove_entry removes a single entry from the address book when multiple matches exist
         Failure/Error: expect(book.entries.size).to eq(1)
    
           expected: 1
                got: 2
    
           (compared using ==)
         # ./spec/address_book_spec.rb:55:in `block (3 levels) in <top (required)>'
    
    Finished in 0.01542 seconds (files took 0.11183 seconds to load)
    8 examples, 2 failures
    
    Failed examples:
    
    rspec ./spec/address_book_spec.rb:41 # AddressBook#remove_entry removes a single entry from the address book
    rspec ./spec/address_book_spec.rb:49 # AddressBook#remove_entry removes a single entry from the address book when multiple matches exist
    
    ........
    
    Finished in 0.00418 seconds (files took 0.12939 seconds to load)
    8 examples, 0 failures
    ```

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,4 +20,19 @@ class AddressBook
     # #11
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    matchedIndex = nil
+    entries.each_with_index do |entry, index|
+      if entry.name == name && entry.phone_number == phone_number && entry.email == email
+        matchedIndex = index
+        break
+      end
+    end
+    if matchedIndex != nil
+      entries.delete_at(matchedIndex)
+    else
+      # Not found
+    end
+  end
 end
